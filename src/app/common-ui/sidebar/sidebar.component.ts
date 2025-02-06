e3753017737ac4cb2894d8c117d0bc3aae8483c5
Evgeniy Pashko <ep.developer@yandex.com>
2025-02-07 02:32:03 +0700

    sidebar menu, logos

--- src/app/common-ui/sidebar/sidebar.component.ts
+++ src/app/common-ui/sidebar/sidebar.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from '../svg-icon/svg-icon.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [SvgIconComponent, RouterLink],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
-
+  menuItems = [
+    { icon: 'home', label: 'Моя страница', link: '' },
+    { icon: 'chat', label: 'Чаты', link: 'chats' },
+    { icon: 'search', label: 'Поиск', link: 'search' },
+  ];
 }
